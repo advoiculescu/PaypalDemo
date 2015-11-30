41878786d0d6c6888afa2ba939d39259226686d9
advoiculescu <advoiculescu@gfi.es>
2015-11-30 10:01:49 +0100

    Segundo commit (Subimos Clase Browser y ObjectMap

---
+++ src/test/java/browser/Browser.java
@@ -0,0 +1,24 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Browser {
+	private static WebDriver driver;
+	
+	public static void startDriver(){
+		driver = new FirefoxDriver();
+	}
+	
+	public static WebDriver driver() {
+		return driver;
+	}
+
+	public static void open(String url) {
+		driver.get(url);
+	}
+
+	public static void close() {
+		driver.close();
+	}
+}
