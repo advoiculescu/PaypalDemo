70f1ef1a4024c9b2b5196b5425321476d0854f3d
Mariano <mfernandez@gfi.es>
2015-11-30 10:04:03 +0100

    Commit_Mariano

---
+++ src/test/java/paypalDemo/RunnerTest.java
@@ -0,0 +1,21 @@
+package paypalDemo;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = { "pretty",
+				"html:target/cucumber",
+				"json:target/cucumber.json",
+		"junit:target/cucumber.xml" },
+		monochrome = true,
+		features = "src/test/resources/" // refer to Feature file
+		)
+
+
+public class RunnerTest {
+
+}
